769bb97b0330be43389760cc31137e5fac3c791b
Kiran Kumar S K <57290570+kirankumarsk25820@users.noreply.github.com>
2025-08-14 18:42:21 +0530

    Update submisssions.js

--- routes/submisssions.js
+++ routes/submisssions.js
@@ -1,23 +1,18 @@
 const express = require('express');
-const Submission = require('../models/Submission');
-const Test = require('../models/Test');
 const router = express.Router();
+const Submission = require('../models/Submission');
 
+// Submit answers
 router.post('/', async (req, res) => {
-  const { candidate, testId, answers } = req.body;
-  const test = await Test.findById(testId).populate('questions');
-  let score = 0;
-  test.questions.forEach((q, i) => {
-    if (q.correctAnswer === answers[i]) score++;
-  });
-  const submission = new Submission({ candidate, test: testId, answers, score });
+  const submission = new Submission(req.body);
   await submission.save();
-  res.send({ score });
+  res.json(submission);
 });
 
+// Get all submissions
 router.get('/', async (req, res) => {
-  const submissions = await Submission.find().populate('candidate test');
-  res.send(submissions);
+  const submissions = await Submission.find();
+  res.json(submissions);
 });
 
 module.exports = router;
